aa04d93c4cf54dac8015e6a81e3332498d461d0f
katagiri-ma <maaya_horiguchi@mail.toyota.co.jp>
2023-12-15 22:47:20 +0900

    create 3table

---
+++ server/db/migrations/20231215111406_create_cookKinds_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("cookKinds", function (table) {
+    table.increments("id").primary();
+    table.string("kindName").notNullable();
+    table.integer("priority").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("cookKinds");
+};
---
+++ server/db/migrations/20231215114411_create_images_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("images", function (table) {
+    table.integer("id").primary();
+    table.string("imagePath").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("images");
+};
