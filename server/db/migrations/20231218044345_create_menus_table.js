ef629badcc6b81a6d8390ed7e671465e7c2dc4d8
katagiri-ma <maaya_horiguchi@mail.toyota.co.jp>
2023-12-18 13:51:48 +0900

    makeMigration menus

---
+++ server/db/migrations/20231218044345_create_menus_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("menus", function (table) {
+    table.increments("id").primary();
+    table.integer("userId").unsigned().references("id").inTable("users");
+    table.integer("foodId").unsigned().references("id").inTable("foods");
+    table.integer("startWeek");
+    table.date("date");
+    table.integer("timingFlag");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("menus");
+};
