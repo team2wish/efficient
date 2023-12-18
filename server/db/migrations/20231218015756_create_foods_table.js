86b00d7959b83ab45326d229630cb08c2e835cb6
katagiri-ma <maaya_horiguchi@mail.toyota.co.jp>
2023-12-18 11:32:10 +0900

    makeMigration foods_table

--- server/db/migrations/20231218015756_create_foods_table.js
+++ server/db/migrations/20231218015756_create_foods_table.js
@@ -3,14 +3,18 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable("users", function (table) {
+  return knex.schema.createTable("foods", function (table) {
     table.increments("id").primary();
-    table.string("userName");
-    table.string("mail").notNullable();
-    table.string("salt");
-    table.string("hash");
-    table.integer("numOfAdults");
-    table.integer("numOfChildren");
+    table.string("name").notNullable();
+    table.boolean("isMain");
+    table.boolean("isSoup");
+    table.boolean("isRice");
+    table
+      .integer("categoryId")
+      .unsigned()
+      .references("id")
+      .inTable("categories");
+    table.integer("genreId").unsigned().references("id").inTable("storeArea");
     table.boolean("shrimp").defaultTo(false);
     table.boolean("crab").defaultTo(false);
     table.boolean("wheat").defaultTo(false);
@@ -26,5 +30,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTable("users");
+  return knex.schema.dropTable("foods");
 };
