ea6b2ed0aa69a02fa74c99b5965c3bf4d413aca7
katagiri-ma <maaya_horiguchi@mail.toyota.co.jp>
2023-12-18 09:47:35 +0900

    create makeMigretion ingredientList&storeArea

---
+++ server/db/migrations/20231218004318_create_storeArea_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("storeArea", function (table) {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("storeArea");
+};
