73cbff2c01e268f8c579e401016cd2a5c31d6a4d
katagiri-ma <maaya_horiguchi@mail.toyota.co.jp>
2023-12-18 14:06:14 +0900

    add  makeMigration recipes_table

--- server/db/migrations/20231218045256_create_recipes_table.js
+++ server/db/migrations/20231218045256_create_recipes_table.js
@@ -18,4 +18,6 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTable("recipes");
+};
