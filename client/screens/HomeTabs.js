64ec0c07e012881aeb99c11a8e799334a89797d6
Satoru <Satoru_OfficePC@SatoruOfficeMacBook-Pro.local>
2023-12-19 19:01:51 +0900

    add:Test recipes

--- client/screens/HomeTabs.js
+++ client/screens/HomeTabs.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import CalendarScreen from "./CalendarScreen";
-import "react-native-gesture-handler";
-import RecipesScreen from "./RecipesScreen";
-import CookProcessScreen from "./CookProcessScreen";
-import ShoppingListScreen from "./ShoppingListScreen";
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CalendarScreen from './CalendarScreen';
+import 'react-native-gesture-handler';
+import RecipesScreen from './RecipesScreen';
+import CookProcessScreen from './CookProcessScreen';
+import ShoppingListScreen from './ShoppingListScreen';
 // import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,7 +18,20 @@ const HomeTabs = () => {
 
     <Tab.Navigator>
       <Tab.Screen name="Calendar" component={CalendarScreen} />
-      <Tab.Screen name="Recipes" component={RecipesScreen} />
+      <Tab.Screen
+        name="Recipes"
+        component={RecipesScreen}
+        // options={{
+        //   title: '献立編集',
+        //   tabBarIcon: () => {
+        //     <MaterialCommunityIcons
+        //       name="calendar-edit"
+        //       size={24}
+        //       color="black"
+        //     />;
+        //   },
+        // }}
+      />
       <Tab.Screen name="ShoppingList" component={ShoppingListScreen} />
       <Tab.Screen name="CookProcess" component={CookProcessScreen} />
     </Tab.Navigator>
