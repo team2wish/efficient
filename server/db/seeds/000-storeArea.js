0707b1bfe8f12eb2f50dd9f25eaeaa54a1285f23
Yoshi2885 <yoshitaka_tanaka_aa@mail.toyota.co.jp>
2023-12-20 13:39:43 +0900

    add: 5 seed files 作成

---
+++ server/db/seeds/000-storeArea.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.seed = function (knex) {
+  return knex("storeArea")
+    .del()
+    .then(function () {
+      return knex("storeArea").insert([
+        { id: 1, name: "青果" },
+        { id: 2, name: "野菜" },
+        { id: 3, name: "豆腐" },
+        { id: 4, name: "魚" },
+        { id: 5, name: "肉" },
+        { id: 6, name: "麺類" },
+        { id: 7, name: "調味料" },
+        { id: 8, name: "米" },
+        { id: 9, name: "乳製品" },
+        { id: 10, name: "卵" },
+        { id: 11, name: "パン類" },
+        { id: 12, name: "その他" },
+      ]);
+    });
+};
