21bdb354719fbe7c176eb9221cc225eee6300aa0
baba <shunichi_baba@mail.toyota.co.jp>
2023-12-27 15:53:53 +0900

    ShoppingList-add:checkbox

--- client/components/ShoppingList.js
+++ client/components/ShoppingList.js
@@ -3,13 +3,15 @@ import { View, Text, StyleSheet } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import recipesApi from "../api/recipesApi";
+import Checkbox from "expo-checkbox";
 
 const ShoppingListScreen = () => {
   const [shoppingList, setShoppingList] = useState();
+  const [isChecked, setIsChecked] = useState({});
 
   const getShoppingList = async () => {
     const res = await recipesApi.getShopping();
-    console.log("data", res.data);
+    // console.log("data", res.data);
     if (res.data) {
       setShoppingList(res.data);
     }
@@ -19,33 +21,39 @@ const ShoppingListScreen = () => {
     getShoppingList();
   }, []);
 
+  const handleValueChange = (id, value) => {
+    setIsChecked({ ...isChecked, [id]: value });
+  };
+
   return (
-    // <View>
-    //   <Text>買い物リスト</Text>
-    //   <Text>12/15〜12/16</Text>
-    // </View>
     <View style={styles.container}>
       <Text style={styles.header__top}>12/18(月) ~ 12/22(金)</Text>
       <GestureHandlerRootView>
         <ScrollView>
           {shoppingList &&
-            shoppingList.map((dateRecipe, index) => {
+            shoppingList.map((dateRecipe, dateIndex) => {
               // console.log("daterecipe2", recipesData);
               return (
-                <View key={index} style={styles.recipes__days}>
+                <View key={dateIndex} style={styles.recipes__days}>
                   <Text style={styles.header__days}>
                     {dateRecipe.store_section}
                   </Text>
 
-                  {dateRecipe.items.map((foodDetail, index) => {
+                  {dateRecipe.items.map((foodDetail, itemIndex) => {
                     // const imgPath = foodDetail.imagePath.slice(0, -4);
+                    const id = `${dateIndex}-${itemIndex}`;
                     return (
-                      <View
-                        style={styles.recipeContainer}
-                        // key={foodDetail.id}
-                        key={index}
-                      >
-                        <Text>{foodDetail.ingredient_name}</Text>
+                      <View style={styles.recipeContainer} key={id}>
+                        <View style={styles.checkboxContainer}>
+                          <Checkbox
+                            style={styles.checkbox}
+                            value={isChecked[id] || false}
+                            onValueChange={(value) =>
+                              handleValueChange(id, value)
+                            }
+                          />
+                          <Text>{foodDetail.ingredient_name}</Text>
+                        </View>
                         <Text style={styles.recipeContainer__right}>
                           {foodDetail.total_quantity}
                           {foodDetail.unit}
@@ -78,6 +86,11 @@ const styles = StyleSheet.create({
     width: 300,
     marginRight: 8,
     // borderWidth: 1,
+  },
+  checkboxContainer: {
+    width: 300,
+    flexDirection: "row",
+    alignItems: "center",
   },
   recipeContainer__right: {
     color: "red",
